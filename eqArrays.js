4c97f937b2d6bbbe61f10886d87a228078ddbdc0
jomicm <singularity.jmcm@gmail.com>
2019-09-17 13:00:33 -0400

    Updating comments

--- eqArrays.js
+++ eqArrays.js
@@ -18,5 +18,5 @@ console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
 console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));// => false
 console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
 console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));// => false
-console.log(assertEqual(eqArrays([], []), true));// => false
+console.log(assertEqual(eqArrays([], []), true));// => true
 console.log(assertEqual(eqArrays([], ["1", "2", 3]), false));// => false
