11c288dc6fd7a6a5c0d65a0b4c94135e7ad59193
jomicm <singularity.jmcm@gmail.com>
2019-09-18 17:40:37 -0400

    Updating file since to complete again since apparently I cut a segment of the code and copied it

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -17,6 +17,14 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !==  arr2.length)  return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
 const ab = { a:'1', b:'2' , d:'3'};
 const ba = { b: '2', a: '1', d:'3'};
 console.log(assertObjectsEqual(ab, ba));
