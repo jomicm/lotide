eea915d99fb87b2bf5f91376042fa186b0d4d2fe
jomicm <singularity.jmcm@gmail.com>
2019-09-18 22:02:04 -0400

    A second version using actual and expected variables

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -7,16 +7,6 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  console.log(`Example label: ${inspect(actual)}`);
-  if (eqObjects(actual, expected)) {
-    return `✅✅✅ ==> Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`;
-  } else {
-    return `❌❌❌ ==> Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`;
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !==  arr2.length)  return false;
   for (let i = 0; i < arr1.length; i++) {
@@ -25,9 +15,20 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  console.log(`Example label: ${inspect(actual)}`);
+  if (actual === expected) {
+    return `✅✅✅ ==> Assertion Passed: [${inspect(actual)}] === [${inspect(expected)}]`;
+  } else {
+    return `❌❌❌ ==> Assertion Failed: [${inspect(actual)}] !== [${inspect(expected)}]`;
+  }
+};
+
 const ab = { a:'1', b:'2' , d:'3'};
 const ba = { b: '2', a: '1', d:'3'};
-console.log(assertObjectsEqual(ab, ba));
+console.log(assertObjectsEqual(eqObjects(ab, ba), true));
 
 const abc = { a: '1', b: '2', z: '3'};
-console.log(assertObjectsEqual(ab, abc));
+console.log(assertObjectsEqual(eqObjects(ab, abc), false));
+
