7ace09548e7f46c12fa8e237138aaf1179377661
jomicm <singularity.jmcm@gmail.com>
2019-09-19 13:15:51 -0400

    Adding eqArrayRecursion function

---
+++ eqArraysRecursion.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION USING TEMPLATE LITERALS
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ ==> Assertion Passed: [${actual}] === [${expected}]`;
+  } else {
+    return `❌❌❌ ==> Assertion Failed: [${actual}] !== [${expected}]`;
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !==  arr2.length)  return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) return false;
+    } else if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)); // => true
+console.log(assertEqual(eqArrays([[2, 3], [4, 5, [2, [2, 3, 4, [[[[[42]]]]]]]]], [[2, 3], [4, 5, [2, [2, 3, 4, [[[[[42]]]]]]]]]), true));// => false
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false));// => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));// => false
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));// => false
+console.log(assertEqual(eqArrays([], []), true));// => true
+console.log(assertEqual(eqArrays([], ["1", "2", 3]), false));// => false
